910aaf561e05688b8284057ff3b07325c9a6e152
BuildTools <jonathanhart1114@gmail.com>
2023-04-06 19:54:23 -0400

    Fixed setting controlling the wrong functionality

--- src/HeavierSpearOptions.cs
+++ src/HeavierSpearOptions.cs
@@ -4,13 +4,13 @@ namespace HeavierSpear
 {
     public sealed class HeavierSpearOptions : OptionInterface
     {
-        public readonly Configurable<bool> flipLizard;
-        public readonly Configurable<bool> modifyStun;
+        public readonly Configurable<bool> FlipLizard;
+        public readonly Configurable<bool> ModifyStun;
 
         public HeavierSpearOptions()
         {
-            flipLizard = config.Bind(nameof(flipLizard), true);
-            modifyStun = config.Bind(nameof(modifyStun), true);
+            FlipLizard = config.Bind(nameof(FlipLizard), true);
+            ModifyStun = config.Bind(nameof(ModifyStun), true);
         }
 
         public override void Initialize()
@@ -19,10 +19,10 @@ namespace HeavierSpear
             var opTab = new OpTab(this, "Options");
             Tabs = new[] { opTab };
             var titleBox = new OpLabel(16f, 560f, "Configuration", true);
-            var flipLizardCheckBox = new OpCheckBox(flipLizard, 16f, 510f);
+            var flipLizardCheckBox = new OpCheckBox(FlipLizard, 16f, 510f);
             flipLizardCheckBox.description = "Causes lizards to flip over upon being hit in the head by a thrown spear. Default is true.";
             var flipLizardLabel = new OpLabel(flipLizardCheckBox.PosX + 40f, flipLizardCheckBox.PosY, "Spears flip lizards");
-            var modifyStunCheckBox = new OpCheckBox(modifyStun, 16f, 460f);
+            var modifyStunCheckBox = new OpCheckBox(ModifyStun, 16f, 460f);
             modifyStunCheckBox.description = "Increases the stun of a spear to the same as a rock. Default is true.";
             var modifyStunLabel = new OpLabel(modifyStunCheckBox.PosX + 40f, modifyStunCheckBox.PosY, "Increased spear stun");
             opTab.AddItems(titleBox, flipLizardCheckBox, flipLizardLabel, modifyStunCheckBox, modifyStunLabel);
